e133f52c8b03096a56fbfa3524412fe7bdb6fb8f
Samir Reddigari <samirreddigari@gmail.com>
2019-10-03 11:54:52 -0400

    feat: add more example leagues

--- src/examples.js
+++ src/examples.js
@@ -5,7 +5,15 @@ export default
         "name": "The Ocho"
     },
     {
+        "id": 2644131,
+        "name": "10-Team"
+    },
+    {
+        "id": 722036,
+        "name": "12-Team (4 Divisions)"
+    },
+    {
         "id": 83168,
-        "name": "Other League"
+        "name": "8-Team (1 Division)"
     }
 ]
