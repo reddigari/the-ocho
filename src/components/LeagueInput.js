1fe58b315ba674807994686773c595fb6a9bb283
Samir Reddigari <samirreddigari@gmail.com>
2019-10-03 11:52:11 -0400

    refactor: move LeagueInput to separate file

---
+++ src/components/LeagueInput.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import { Row, Col, Button, Modal, FormControl, Alert } from 'react-bootstrap';
+import exampleLeagues from '../examples.js';
+import '../styles/LeagueInput.css';
+
+function LeagueInput(props) {
+  const [show, setShow] = useState(props.show);
+  const [leagueId, setLeagueId] = useState(null);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  function submit() {
+      setShow(false);
+      props.changeLeague(leagueId);
+  }
+  function setAndSubmit(evt, id) {
+      evt.preventDefault();
+      setShow(false);
+      props.changeLeague(id);
+  }
+
+  return (
+    <>
+        <span className="change-league text-primary" onClick={handleShow}>Change</span>
+
+        <Modal show={show} onHide={handleClose}>
+            <Modal.Header>
+                <Modal.Title><h2>Choose an ESPN League</h2></Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                
+                {props.errorMsg ? <Alert variant="danger">{props.errorMsg}</Alert> : null}
+
+                <FormControl
+                    size="lg"
+                    placeholder="Enter League ID"
+                    onChange={e => setLeagueId(e.target.value)}
+                />
+                <Row>
+                    <Col>
+                        <div className="league-option-title">Or select an example league:</div>
+                        {
+                            exampleLeagues.map(function(lg) {
+                                const { name, id } = lg;
+                                return (
+                                    <a className="league-option" href="#" key={id}
+                                       onClick={(evt) => setAndSubmit(evt, id)}>
+                                        {name} &mdash; {id}
+                                    </a>
+                                )
+                            })
+                        }
+                    </Col>
+                </Row>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="danger" onClick={handleClose} disabled={!props.allowCancel}>
+                    Cancel
+                </Button>
+                <Button variant="success" onClick={submit} disabled={leagueId===null}>
+                    Go
+                </Button>
+            </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
+
+export default LeagueInput;
--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,68 +1,7 @@
-import React, { useState } from 'react';
-import { Row, Col, Button, Modal, FormControl } from 'react-bootstrap';
-import exampleLeagues from '../examples.js';
+import React from 'react';
+import LeagueInput from './LeagueInput.js';
 import '../styles/TopBar.css';
 
-
-function LeagueInput(props) {
-  const [show, setShow] = useState(props.show);
-  const [leagueId, setLeagueId] = useState(null);
-
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-  function submit() {
-      props.changeLeague(leagueId);
-      setShow(false);
-  }
-  function setAndSubmit(evt, id) {
-      evt.preventDefault();
-      props.changeLeague(id);
-      setShow(false);
-  }
-
-  return (
-    <>
-        <span className="change-league text-primary" onClick={handleShow}>Change</span>
-
-        <Modal show={show} onHide={handleClose}>
-            <Modal.Header closeButton>
-                <Modal.Title><h2>Choose an ESPN League</h2></Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <FormControl
-                    size="lg"
-                    placeholder="Enter League ID"
-                    onChange={e => setLeagueId(e.target.value)}
-                />
-                <Row>
-                    <Col>
-                        <div className="league-option-title">Or select an example league:</div>
-                        {
-                            exampleLeagues.map(function(lg) {
-                                const { name, id } = lg;
-                                return (
-                                    <a className="league-option" href=""
-                                       onClick={(evt) => setAndSubmit(evt, id)}>
-                                        {name} - {id}
-                                    </a>
-                                )
-                            })
-                        }
-                    </Col>
-                </Row>
-            </Modal.Body>
-            <Modal.Footer>
-                <Button variant="danger" onClick={handleClose} disabled={!props.allowCancel}>
-                    Cancel
-                </Button>
-                <Button variant="success" onClick={submit} disabled={leagueId===null}>
-                    Go
-                </Button>
-            </Modal.Footer>
-      </Modal>
-    </>
-  );
-}
 
 function TopBar(props) {
     
