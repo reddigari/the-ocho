a737fc869ef0f767bd453c40416555b664609efc
Samir Reddigari <samirreddigari@gmail.com>
2019-10-06 12:16:44 -0400

    fix: improve JSX in help modals

--- src/components/HelpModal.js
+++ src/components/HelpModal.js
@@ -18,7 +18,7 @@ function HelpModal(props) {
                 <Modal.Title><h2>{props.title}</h2></Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                <p>{props.text}</p>
+                {props.body}
             </Modal.Body>
       </Modal>
     </>
@@ -26,24 +26,38 @@ function HelpModal(props) {
 }
 
 function LuckIndexHelp(props) {
-    const msg = [
-        <b>Luck Index</b>, " measures how lucky a team has been, where ",
-        <u>lucky teams have positive values</u>, " and  ", <u>unlucky teams have negative values</u>, ".", 
-        <br />, <br />, "It is equal to a team's wins minus their expected wins, determined by weekly score ranking. ",
-        <br />, <br />, <b>Example</b>, ": for some week in an  8-team league, the second highest scorer would have defeated ",
-        "6 of the 7 other teams, giving 6/7 = 0.857 expected wins. The lowest scorer would have defeated no teams, giving 0 expected wins. ",
-        "Doing this each week and summing the values ",
-        "gives a teams total expected wins. A lucky team will have more wins than expected, giving a positive luck index."]
-    return <HelpModal title="Luck Index" text={msg} />
+    const msg = (
+        <div>
+            <b>Luck Index</b>  measures how lucky a team has been, where
+            <u>lucky teams have positive values</u> and <u>unlucky teams
+            have negative values</u>.
+            <br /><br />
+            It is equal to a team's wins minus their expected wins,
+            determined by weekly score ranking.
+            <br /><br />
+            <b>Example</b>: For some week in an  8-team league, the second
+            highest scorer would have defeated  6 of the 7 other teams, giving
+            6/7 = 0.857 expected wins. The lowest scorer would have defeated no
+            teams, giving 0 expected wins. Doing this each week and summing
+            the values gives a teams total expected wins. A lucky team will
+            have more wins than expected, giving a positive luck index.
+        </div>
+    )
+    return <HelpModal title="Luck Index" body={msg} />
 }
 
 function PointsWinsHelp(props) {
-    const msg = [
-        "Choosing ", <b>Points-based Wins</b>, " assigns an additional win to each team scoring in the top half ",
-        "of scores that week, and an additional loss to those scoring in the bottom half.", <br/>, <br/>,
-        "It is designed to reduce the unfairness of very high-scoring losses and very low-scoring wins."
-    ]
-    return <HelpModal title="Points-based Wins" text={msg} />
+    const msg = (
+        <div>
+            Choosing <b>Points-based Wins</b> assigns an additional win
+            to each team scoring in the top half of scores that week,
+            and an additional loss to those scoring in the bottom half.
+            <br /><br />
+            It is designed to reduce the unfairness of very high-scoring
+            losses and very low-scoring wins.
+        </div>
+    )
+    return <HelpModal title="Points-based Wins" body={msg} />
 }
 
 export { LuckIndexHelp, PointsWinsHelp };
