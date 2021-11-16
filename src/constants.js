6482b18116db2376c87c1ef8dec65ad1772590df
Samir Reddigari <samirreddigari@gmail.com>
2021-11-16 10:56:42 -0500

    fix: update endpoint to 2021 season

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 const DATA_URL = "https://fantasy.espn.com/apis/v3/games/ffl/seasons/" 
-                 + "2019/segments/0/leagues/<LEAGUE_ID>?view=mBoxscore"
+                 + "2021/segments/0/leagues/<LEAGUE_ID>?view=mBoxscore"
                  + "&view=mMatchupScore&view=mSchedule&view=mScoreboard"
                  + "&view=mSettings&view=mStatus&view=mTeam&view=modular"
                  + "&view=mNav";
